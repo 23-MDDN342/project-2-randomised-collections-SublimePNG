07f3ad4e96f47465746fb31cb421250a6ef024ca
SublimePNG <80139430+SublimePNG@users.noreply.github.com>
2023-05-01 17:34:29 +1200

    Face rough draft

--- face_code.js
+++ face_code.js
@@ -13,12 +13,13 @@
  * eye_value is an integer number of eyes: either 0, 1, 2, or 3
  * mouth_value is how open the mouth is and should generally range from 0.5 to 10
  */
-function orangeAlienFace() {
+function MAINFACE() {
+  
  
   beginShape();
-  vertex(-2,10);
-  vertex(2,10);
-  vertex(6,6);
+  vertex(-2,10);//chin L
+  vertex(2,10);//chin R
+  vertex(6,6);//Jaw R
   vertex(7,2);
   vertex(7,-3);
   vertex(6,-7);
@@ -28,8 +29,24 @@ function orangeAlienFace() {
   vertex(-6,-7);
   vertex(-7,-3);
   vertex(-7,2);
-  vertex(-6,6);
+  vertex(-6,6);//Jaw L
   endShape(CLOSE);
+//eyes
+  rect(2,-1.5,3,2)
+  rect(-2,-1.5,-3,2)
+//brows
+  rect(1,-3,5,1)
+  rect(-1,-3,-5,1)
+
+//nose
+rect(-1.5,-2,3,5)
+//mouth
+rect(-3,4.5,6,2)
+//ears
+rect(6.5,-1.5,2.5,5)
+rect(-6.5,-1.5,-2.5,5)
+
+  
   
 }
 

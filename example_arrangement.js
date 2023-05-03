c89445906337833004dba415ceeb57676f1f0434
SublimePNG <80139430+SublimePNG@users.noreply.github.com>
2023-05-04 10:51:09 +1200

    Gaussian Selection in example_arrangement

--- example_arrangement.js
+++ example_arrangement.js
@@ -26,10 +26,24 @@ function changeRandomSeed() {
 }
 
 // global variables for colors
-const bg_color1 = [71, 222, 219]
+const bg_color1 = [77, 113, 135]
 
 function mouseClicked() {
   changeRandomSeed();
+}
+
+function getAveragedRandom(min, max, n) {
+
+ let sum = 0;
+
+ for(let i=0; i<n; i++) {
+
+ sum = sum + random(min, max);
+
+ }
+
+ return sum / n;
+
 }
 
 function draw () {
@@ -46,42 +60,65 @@ function draw () {
 
   // draw a 7x4 grid of faces
   let w = canvasWidth / 7;
-  let h = canvasHeight / 4;
+  let h = canvasHeight / 3;
   for(let i=0; i<4; i++) {
     for(let j=0; j<7; j++) {
       let y = h/2 + h*i;
       let x = w/2 + w*j;
-      if (i == 0) {
+     
         // center face
-        let eye_value = 2;
-        let tilt_value = random(-45, 45);
-        let mouth_value = random(1, 3);
-        let is_cyclops = random(0, 100);
-        if(is_cyclops < 10) {
-          eye_value = 1;
-          tilt_value = random(-5, 5);
-          mouth_value = random(5, 10);
-        }
+      
+        //let is_cyclops = random(0, 100);
+
+        //if(is_cyclops < 10) {
+         // eye_value = 1;
+         // tilt_value = random(-5, 5);
+         // mouth_value = random(0, 1.7);
+        //}
+       
         push();
+        //let headSize = (random(-1.5,1.3))
         translate(x, y);
-        scale(w/25, h/25);
-        MAINFACE(tilt_value, eye_value, mouth_value);
-        pop();
-      }
-      else if (i > 0) {
-        // all other faces
-        push();
-        translate(x, y);
-        scale(w/25, h/25);
-        if((i+j)%2 == 0) {
-          simplePurpleFace();
+
+        scale(5, 5);
+        //scale(10+headSize, 10+headSize);
+        //let randomFaceColour = int(random(0,3))
+        
+        let faceWidth = random(-0.5,0.5);
+        let jawWidth = random(-2,2);
+        let chinWidth = random(-1, 2);
+        let chinLength = random(0,1);
+        
+        let mouthY = random(-0.7,1.5);
+        let mouthSize = random(-1,1);
+        
+        let noseWidth = random (-0.5,0.5);
+        let noseLength = random (-0.3,0.3);
+
+        let eyeX = random (-.03,0.3);
+        let eyeY = random(-1,1);
+        let eyeWidth= random(-0.5,0.5);
+        let eyeHeight = random(-0.5,0.5);
+
+        let browY = random(-1,1);
+        let browThick = random(-0.5,1);
+
+        let headHeight = random(-1,1);
+
+        let SpinnerValue = random(0,100)
+        let randomFaceColour = int(random(0,3))
+
+        if(SpinnerValue < 50){
+          randomFaceColour = 0;
         }
-        else {
-          thinness_value = random(0, 100);
-          blockyFace(thinness_value);
+        else{
+          randomFaceColour = int(random(1,3))
         }
+        //MAINFACE(tilt_value, eye_value, mouth_value);
+  
+        MAINFACE(faceWidth, jawWidth, chinWidth, chinLength, mouthY, mouthSize, noseWidth, noseLength, eyeX, eyeY, eyeWidth, eyeHeight, browY, browThick, headHeight, randomFaceColour);
         pop();
-      }
+        
     }
   }
 }
